401217792b9d39e6068623e9afbfcc8439d150fb
BRICIO DE JESUS SANCHEZ <bricio3599@gmail.com>
2024-11-08 11:09:06 -0600

    MICROSERVICIO REST CON ASP.NET CORE WEP API

---
+++ CoreWebApiPersona/Data/CoreWebApiPersonaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CoreWebApiPersona.Modelo;
+
+namespace CoreWebApiPersona.Data
+{
+    public class CoreWebApiPersonaContext : DbContext
+    {
+        public CoreWebApiPersonaContext (DbContextOptions<CoreWebApiPersonaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoreWebApiPersona.Modelo.Persona> Persona { get; set; }
+    }
+}
---
+++ CoreWebApiPersona/Modelo/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApiPersona.Modelo
+{
+    public class Persona
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string fecha { get; set; }
+    }
+}
